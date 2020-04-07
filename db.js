fcaf83f952370e7f9be4874e4d6553ad8b00e8fd
Stephanie <stephaniesmith12514@gmail.com>
2020-04-07 14:38:45 +0000

    Changes db

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-      //   connectionString: process.env.DATABASE_URL,
-      // });
 const pool = new Pool({
-  host: '/var/run/postgresql',
-  user: 'ubuntu',
-  database: 'todos',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 });
+// const pool = new Pool({
+//   host: '/var/run/postgresql',
+//   user: 'ubuntu',
+//   database: 'todos',
+//   port: 5432,
+// });
 
 
 module.exports = {
