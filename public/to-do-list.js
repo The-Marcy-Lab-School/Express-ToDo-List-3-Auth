502b7ea8b32196612e06fccea9258928d7a5d4b4
Enmanuel <edelanuezlang@gmail.com>
2020-04-07 08:20:44 +0000

    test commit

--- public/to-do-list.js
+++ public/to-do-list.js
@@ -7,7 +7,29 @@ window.addEventListener('load', (event) => {
     .then(tasks => {
         const tasksArray = tasks.results
         for(let task of tasksArray){
-            console.log(task)
+            if(task.is_complete === false){
+                complete.appendChild(document.createElement('div').classList.add("task").innerHTML = `
+                        <h3 class="name">${task.name}</h3>
+                        <p class="description">${task.description}</p>
+                        <p class="dateAdded">Added: ${task.date_added}</p>
+                        <button class="button button-outline">Complete</button>
+                        <button class="button">Delete</button>
+                        <button class="button button-clear">Update</button>
+            `)
+            }
+            
+            if(task.is_complete === true){
+                incomplete.appendChild(document.createElement('div').classList.add("task").innerHTML = `
+                        <h3 class="name">${task.name}</h3>
+                        <p class="description">${task.description}</p>
+                        <p class="dateAdded">Added: ${task.date_added}</p>
+                        <p class="dateCompleted">${task.date_complete}</p>
+                        <button class="button button-outline">Complete</button>
+                        <button class="button">Delete</button>
+                        <button class="button button-clear">Update</button>
+            `)
+            }
+            
         }
     })
 })
