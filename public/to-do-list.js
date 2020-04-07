2a1eb9370036d765048227a43255a6d2b1ec11e4
Enmanuel <edelanuezlang@gmail.com>
2020-04-07 07:39:11 +0000

    test commit

--- public/to-do-list.js
+++ public/to-do-list.js
@@ -2,5 +2,7 @@ const incomplete = document.getElementById('incomplete')
 const complete = document.getElementById('complete')
 
 window.addEventListener('load', (event) => {
-    fetch('/tasks').then(res => console.log(res.json()))
+    fetch('/tasks')
+    .then(res => res.json())
+    .then(tasks => console.log(tasks))
 })
