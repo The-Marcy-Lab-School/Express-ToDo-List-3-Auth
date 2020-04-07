9d4faac8aade6482b4871a259f457eaf3be5afcb
Enmanuel <edelanuezlang@gmail.com>
2020-04-07 07:32:42 +0000

    added completed views

--- public/to-do-list.js
+++ public/to-do-list.js
@@ -1,3 +1,6 @@
-const incomplete = document.getElementById('incomplete-tasks')
+const incomplete = document.getElementById('incomplete')
+const complete = document.getElementById('complete')
 
-fetch('https://5a90f1b3156e47f4b0e59404a9162ead.vfs.cloud9.us-east-2.amazonaws.com/').then((res) => res.json()).then((json) => incomplete.innerText = json)
+window.addEventListener('load', (event) => {
+    fetch('/tasks').then(res => console.log(res.json()))
+})
