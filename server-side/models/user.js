98d322f76c5c9c4440b99f99ebd30bce562d53bc
Enmanuel <devonteduncan202@gmail.com>
2020-04-08 09:12:45 +0000

    test commit

--- server-side/models/user.js
+++ server-side/models/user.js
@@ -8,12 +8,11 @@ async function addUser(first_name, last_name, email, hashedPassword) {
 }
 
 function getByEmail(email) {
-  const queryText = 'SELECT * FROM users WHERE email = $1'
-  const client = db.connect()
-  client.query(queryText, [email])
+  const queryText = "SELECT * FROM users WHERE email = '$1'"
+  
+  db.query(queryText, [email])
     .then((data) => {
       console.log(data[0])
-      client.release()
       return data.rows[0]
     })
 }
