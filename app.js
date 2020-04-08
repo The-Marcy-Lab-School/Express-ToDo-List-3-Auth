5dc57191002cc017e1b3fe622da89569a6a32655
Enmanuel de la Nuez <enmanuel@et450.home>
2020-04-08 09:50:19 -0400

    require all routes, use auth middleware

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const signUp = require('./routes/signUp');
+const logIn = require('./routes/logIn');
+const tasks = require('./routes/tasks');
+const authenticate = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,6 +16,15 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 app.use(signUp);
+app.use(logIn);
+
+app.use(authenticate);
+
+app.use(tasks);
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
