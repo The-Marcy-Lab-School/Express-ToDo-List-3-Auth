1e672e41568f8e730717253f5e8f583d20c08dc6
Stephanie <stephaniesmith12514@gmail.com>
2020-04-08 04:21:33 +0000

    Sets database to heroku

--- db.js
+++ db.js
@@ -1,15 +1,8 @@
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-// });
 const pool = new Pool({
-  host: '/var/run/postgresql',
-  user: 'ubuntu',
-  database: 'todos',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 });
-
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
