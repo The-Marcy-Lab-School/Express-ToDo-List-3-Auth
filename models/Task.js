61b3fed55da646f7557870b98aa3d187e5364a16
Carmen Salas <carmensalas@Carmens-MacBook-Pro.local>
2020-04-09 14:43:44 -0400

    updated queries in task class

--- models/Task.js
+++ models/Task.js
@@ -7,29 +7,39 @@ class Task {
     dateCreated,
     dueDate,
     isComplete,
+    userId,
   ) {
-    const queryText = 'INSERT INTO task (task_name, task_description, date_created, due_date, is_complete) VALUES($1,$2,$3,$4,$5)';
+    const queryText = 'INSERT INTO task (task_name, task_description, date_created, due_date, is_complete, user_id) VALUES($1,$2,$3,$4,$5)';
     return db.query(queryText, [
       taskName,
       taskDescription,
       dateCreated,
       dueDate,
       isComplete,
+      userId,
     ]);
   }
 
-  static getAllTasks() {
-    return db.query('SELECT * FROM task;');
+  static getAllTasks(userId) {
+    return db.query('SELECT * FROM task;', [userId]);
   }
 
-  static getLastCreated() {
+  static getTaskById(userId, id) {
+    const queryText = 'SELECT * FROM task WHERE user_id = $1 AND id = $2;';
+    return db.query(queryText, [userId, id])
+      .then((res) => res.rows[0])
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static getLastCreated(id) {
     const queryText = 'SELECT * FROM task ORDER BY id DESC LIMIT 1;';
-    return db.query(queryText);
-  }
-
-  static getTaskById(id) {
-    const queryText = 'SELECT * FROM task WHERE id = $1;';
-    return db.query(queryText, [id]);
+    return db.query(queryText, [id])
+      .then((res) => res.rows[0])
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   static completeTask(id) {
